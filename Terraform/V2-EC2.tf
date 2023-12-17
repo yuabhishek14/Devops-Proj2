c767421338aadb0e6880bb38b089121f4739a799
Abhishek Tripathy <yuabhishek14@gmail.com>
2023-12-17 11:00:14 +0530

    added terraform file to create security group for ec2

---
+++ Terraform/V2-EC2.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "demo-server" {
+  ami = "ami-0759f51a90924c166"
+  instance_type = "t2.micro"
+  key_name = "dpp"
+  security_groups = [ "demo-sg" ]
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+
+  ingress {
+    description      = "SSH Access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh-port"
+  }
+}
