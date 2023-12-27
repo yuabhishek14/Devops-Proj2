5e28074b034f5979ec3d1a78deed8293576151db
Abhishek Tripathy <yuabhishek14@gmail.com>
2023-12-27 22:21:00 +0530

    modification

--- Terraform/V4-EC2-WITH_VPC_for_each.tf
+++ Terraform/V4-EC2-WITH_VPC_for_each.tf
@@ -28,6 +28,14 @@ resource "aws_security_group" "demo-sg" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description      = "Jenkins Port"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
